f542a866473e475c84b68e0016ac8f513eee9e55
Hilary29 <keubouhilary@gmail.com>
2025-05-09 15:34:45 +0100

    update images

--- src/app/Trainings/page.tsx
+++ src/app/Trainings/page.tsx
@@ -1,22 +1,21 @@
-import TopNavbar from "../home/components/TopNavbar"
-import Navbar from "../home/components/Navbar"
-import Landing_page from "./Landing_page"
-import Training from "../home/components/Trainings"
-import Software from "../home/components/Software"
-import Special from "./Special"
-import Footer from "../home/components/Footer"
-
+import TopNavbar from "../home/components/TopNavbar";
+import Navbar from "../home/components/Navbar";
+import Landing_page from "./Landing_page";
+import Training from "../home/components/Trainings";
+import Software from "../home/components/Software";
+import Special from "./Special";
+import Footer from "../home/components/Footer";
 
 export default function Home() {
-    return (
-        <div>
-        <TopNavbar/>
-        < Navbar/> 
-        <Landing_page/> 
-        < Training/> 
-        < Software/> 
-        < Special/>
-        < Footer/>
-         </div>
-    );
-    }
+  return (
+    <div>
+      <TopNavbar />
+      <Navbar />
+      <Landing_page />
+      <Training />
+      <Software />
+      <Special />
+      <Footer />
+    </div>
+  );
+}
