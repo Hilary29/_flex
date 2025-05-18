df58e141c9b832740b06f91e51d5e4b254648723
Hilary29 <keubouhilary@gmail.com>
2025-05-18 04:37:28 +0100

    Add language existing components

--- src/app/[locale]/language/page.tsx
+++ src/app/[locale]/language/page.tsx
@@ -1,31 +1,44 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Header2 from '../home/components/Header2'
-import TopNavbar from '../home/components/TopNavbar'
-import Footer from '../home/components/Footer'
-import FAQ from '../home/components/FAQ'
-import Testimonials from '../home/components/Testimonials'
-import Tech_Products from '../home/components/TechProducts'
+import React from "react";
+import Header2 from "../home/components/Header2";
+import TopNavbar from "../home/components/TopNavbar";
+import Footer from "../home/components/Footer";
+import FAQ from "../home/components/FAQ";
+import Testimonials from "../home/components/Testimonials";
+/* import Tech_Products from '../home/components/TechProducts'
 import VIP_trainings from '../home/components/vip-trainings'
-import Hero from '../home/components/Hero'
-
+ */
+import Hero from "../home/components/Hero";
+import FeatureShowcase from "../home/components/FeatureShowcase";
+import LanguageGallery from "../home/components/LanguageGallery";
+import AboutPage from "../home/components/AboutPage";
+import ImmigrationPage from "../home/components/ImmigrationPage";
+import StatisticsPage from "../home/components/StatisticsPage";
+/* import TestimonialPage from '../home/components/TestimonialPage' */
 
 const Page = () => {
   return (
     <div className="min-h-screen">
       <TopNavbar />
       <Header2 />
-      <div className='mt-0'>
+      <div className="mt-0">
         <Hero />
       </div>
-      <VIP_trainings />
-      <Tech_Products />
+      <FeatureShowcase />
+      <LanguageGallery />
+      <AboutPage />
+      <ImmigrationPage />
+      {/*       <TestimonialPage/> */}
+
+      {/*       <VIP_trainings />
+      <Tech_Products /> */}
       <Testimonials />
+      <StatisticsPage/>
       <FAQ />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
